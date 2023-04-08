08d4f1210d68f69cfc1531a65bf174e54f658edd
Samir Jadhav <samirzjadhav@gmail.com>
2023-04-08 11:31:20 +0530

    feat: added script for section

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ function linkAction() {
   navMenu.classList.Remove(show);
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+const sections = document.querySelectorAll("section[id]");
+window.addEventListener("scroll", scrollActive);
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelectorAll(
+        ".nav_menu a[href*=" + sectionId + "]".classList.add("active")
+      );
+    } else {
+      document.querySelectorAll(
+        ".nav_menu a[href*=" + sectionId + "]".classList.remove("active")
+      );
+    }
+  });
+}
