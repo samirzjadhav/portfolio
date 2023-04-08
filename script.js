b317ec99c43df8c71aec0760e298906b083ef65f
Samir Jadhav <samirzjadhav@gmail.com>
2023-04-08 15:59:43 +0530

    feat: added intervel to all section

--- script.js
+++ script.js
@@ -41,3 +41,36 @@ function scrollActive() {
     }
   });
 }
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+/*SCROLL HOME*/
+sr.reveal(".home_title", {});
+sr.reveal(".home_scroll", { delay: 200 });
+sr.reveal(".home_img", { origin: "right", delay: 400 });
+
+/*SCROLL ABOUT*/
+sr.reveal(".about_img", { delay: 500 });
+sr.reveal(".about_subtitle", { delay: 300 });
+sr.reveal(".about_profession", { delay: 400 });
+sr.reveal(".about_text", { delay: 500 });
+sr.reveal(".about_social-icon", { delay: 600, interval: 200 });
+
+/*SCROLL SKILLS*/
+sr.reveal(".skills_subtitle", {});
+sr.reveal(".skills_name", { distance: "20px", delay: 50, interval: 100 });
+sr.reveal(".skills_img", { delay: 400 });
+
+/*SCROLL PORTFOLIO*/
+sr.reveal(".portfolio_img", { interval: 200 });
+
+/*SCROLL CONTACT*/
+sr.reveal(".contact_subtitle", {});
+sr.reveal(".contact_text", { interval: 200 });
+sr.reveal(".contact_input", { delay: 400 });
+sr.reveal(".contact_button", { delay: 600 });
